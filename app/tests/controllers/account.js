31eb2eb56388a4b397b824e27785d29d738c52ac
John P Tomanelli II <jptomanelli@gmail.com>
2016-09-21 14:16:07 -0400

    Create controllers/account.js
    Note this is not currently working.

---
+++ app/tests/controllers/account.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../../app.js');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Account Controller',function() {
+  it('Page Has correct status and is HTML', function(done) {
+    chai.request(server);
+    chai.get('/account');
+    chai.end(function (err,res) {
+      expect(res).to.have.status(200);
+      expect(req).to.be.html;
+      done();
+    });
+  });
+});
