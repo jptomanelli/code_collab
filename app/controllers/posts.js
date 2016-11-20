64ded77d53e4cda5059dbdcc6472a91e13ea0b3d
John P Tomanelli II <jptomanelli@gmail.com>
2016-11-19 21:23:43 -0500

    Update post controller
    Post page will now show all posts.

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -3,11 +3,15 @@
 //  This page will need post data to fill
 
 const express = require('express');
+const models = require('../models');
+const Redirect = require('../middlewares/redirect');
 const router = express.Router();
 
 //  need to send to views
 router.get('/', (req,res) => {
-  res.render('posts');
+  models.Posts.findAll().then((posts) => {
+    res.render('posts', {posts});
+  });
 });
 
 router.get('/:title', (req, res) => {
