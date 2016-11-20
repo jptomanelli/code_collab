0fc8f705ac63adeb782b342e585688b8c71e873a
John P Tomanelli II <jptomanelli@gmail.com>
2016-11-19 21:23:10 -0500

    Update Create page
    Added in creating a post functionality. You can now create a simple
    post.

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -2,10 +2,26 @@
 //  User would be able to create a post here
 
 const express = require('express');
+const models = require('../models');
 const router = express.Router();
 
 router.get('/', (req,res) => {
   res.render('create');
+});
+
+router.post('/', (req,res) => {
+  models.Posts.create({
+    title: req.body.title,
+    link: req.body.link,
+    language:req.body.lang,
+    description:req.body.desc,
+  }).then(function(Posts) {
+    res.redirect(`/posts/${Posts.title}`);
+  }).catch((e) => {
+    res.redirect('/');
+    //  Print error
+    console.log(e);
+  })
 });
 
 module.exports = router;
