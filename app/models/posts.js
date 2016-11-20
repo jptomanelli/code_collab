61f8d92c48a024de3fb8f1af8e2ae3549b22dda4
John P Tomanelli II <jptomanelli@gmail.com>
2016-11-19 21:19:37 -0500

    Update models
    User has many posts

---
+++ app/models/posts.js
@@ -0,0 +1,39 @@
+'use strict';
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  var Posts = sequelize.define('Posts', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    title: {
+      type: Sequelize.STRING,
+    },
+    link: {
+      type: Sequelize.STRING,
+    },
+    language: {
+      type: Sequelize.INTEGER,
+    },
+    description: {
+      type: Sequelize.TEXT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }, {
+    classMethods: {
+      associate: function(models) {
+        this.belongsTo(models.User, {foreignKey: 'id'});
+      }
+    }
+  });
+  return Posts;
+};
